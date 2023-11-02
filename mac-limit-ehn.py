e66e828b6174f7f0ed65fcb752132e1e1e182728
Satoshi Ogawa <35554139+saogawa@users.noreply.github.com>
2023-11-03 07:41:59 +0900

    Update mac-limit-ehn.py

--- mac-limit-ehn.py
+++ mac-limit-ehn.py
@@ -1,12 +1,20 @@
 import sys
 import json
 
-def count_active_entries(path):
-    return int(path.get("value", 0 ))
+"""
+This script compares the number of active MAC entries to the MAC limit for each subinterface.
+If the number of active entries exceeds the configured MAC limit, the corresponding subinterface
+is administratively disabled to prevent further MAC address learning.
 
-def get_max_entries(options):
-    return int(options.get("max-entries", 0))
+The comparison is done by parsing the JSON data which contains the paths and values for
+the 'bridge-table mac-limit maximum-entries' and 'bridge-table statistics active-entries'
+for each subinterface. The script identifies each subinterface by its unique path and then
+compares the values. If the 'active-entries' value is greater than the 'maximum-entries' value,
+a command to administratively disable the subinterface is generated.
 
+Note: The actual disabling of the subinterface is not performed by this script and should be
+handled by the network management system that executes the script's output commands.
+"""
 
 # main entry function for event handler
 def event_handler_main(in_json_str):
@@ -14,25 +22,42 @@ def event_handler_main(in_json_str):
     in_json = json.loads(in_json_str)
     paths = in_json["paths"]
     options = in_json["options"]
-    
-    max_learn_mac = get_max_entries(options)
+
+    # Create a dictionary to store the values for comparison
+    entries_dict = {}
+
+    # Iterate through the paths and populate the dictionary
+    for entry in paths:
+        # Split the path to get the key and value type
+        key, value_type = entry["path"].split(" bridge-table ")
+
+        # Convert the value to integer
+        value = int(entry["value"])
+
+        # If the key is not in the dictionary, initialize it with an empty dictionary
+        entries_dict.setdefault(key, {})
+
+        # Add the value type and value to the dictionary
+        entries_dict[key][value_type.split(" ")[1]] = value
+
     response_actions = []
 
-    for path in paths:
-        learn_mac = count_active_entries(path)
-        instance = path['path'].replace(' bridge-table statistics active-entries', '')
-
-        if  learn_mac > max_learn_mac:
-            if options.get("debug") == "true":
-                print ("Port shutdown = ", instance)
-            response_actions.append(
-                {
-                    "set-cfg-path": {
-                    "path": f"{instance} admin-state",
-                    "value": "disable",
-		    }
-                }
-            )
+    # Iterate through the dictionary
+    for key, values in entries_dict.items():
+        # If both "active-entries" and "maximum-entries" are in the dictionary
+        if "active-entries" in values and "maximum-entries" in values:
+            # If the number of active entries is greater than the maximum entries
+            if values["active-entries"] > values["maximum-entries"]:
+                response_actions.append(
+                    {
+                        "set-cfg-path": {
+                            "path": f"{key} admin-state",
+                            "value": "disable",
+                            }
+                    }
+                )
+                if options.get("debug") == "true":
+                    print(f"Shutdown port {key}")
 
     response = {"actions": response_actions}
     return json.dumps(response)
@@ -43,23 +68,38 @@ def event_handler_main(in_json_str):
 def main():
     example_in_json_str = """
 {
-    "paths": [
-        {
-            "path":"interface ethernet-1/1 subinterface 10  bridge-table statistics active-entries",
-            "value":"6"
-        },
-        {
-            "path":"interface ethernet-1/2 subinterface 10  bridge-table statistics active-entries",
-            "value":"6"
-        }
-    ],
-    "options": {
-        "max-entries":3,
-        "debug": "true"
+  "paths": [
+    {
+      "path": "interface ethernet-1/1 subinterface 10 bridge-table statistics active-entries",
+      "value": "9"
     },
-    "persistent-data": {"last-state":"up"}
+    {
+      "path": "interface ethernet-1/1 subinterface 10 bridge-table mac-limit maximum-entries",
+      "value": "8"
+    },
+    {
+      "path": "interface ethernet-1/10 subinterface 11 bridge-table statistics active-entries",
+      "value": "10"
+    },
+    {
+      "path": "interface ethernet-1/10 subinterface 11 bridge-table mac-limit maximum-entries",
+      "value": "6"
+    },
+    {
+      "path": "interface ethernet-1/11 subinterface 20 bridge-table statistics active-entries",
+      "value": "9"
+    },
+    {
+      "path": "interface ethernet-1/11 subinterface 20 bridge-table mac-limit maximum-entries",
+      "value": "8"
+    }
+  ],
+  "options": {
+    "debug": "true"
+  }
 }
 """
+    
     json_response = event_handler_main(example_in_json_str)
     print(f"Response JSON:\n{json_response}")
 
