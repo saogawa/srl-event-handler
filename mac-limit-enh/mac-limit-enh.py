bb135f11564d464240cce5cb6154c6c5ed005449
Satoshi Ogawa <35554139+saogawa@users.noreply.github.com>
2024-02-27 15:07:35 +0900

    Update mac-limit-enh.py

--- mac-limit-enh/mac-limit-enh.py
+++ mac-limit-enh/mac-limit-enh.py
@@ -8,23 +8,30 @@ with open('/var/log/srlinux/file/mac-limit', 'r') as file:
 # Step 2: Process the lines
 new_lines = []
 for line in lines:
-    if re.search(r'The number of MAC addresses in the bridge table for the sub-interface .* has reached the allowed limit of .*', line):
-        # Parse the interface name from the string
-        match = re.search(r'ethernet-(\d+/\d+).(\d+)', line)
-        interface = match.group(1)
-        subinterface = match.group(2)
+    lower_line = line.lower()
+    if re.search(r'the number of mac addresses in the bridge table for the sub-interface .* has reached the allowed limit of .*', lower_line):
+        # Check if the limit is 8192, if so, do nothing
+        if "reached the allowed limit of 8192" in lower_line:
+            new_lines.append(line)
+        else:
+            # Parse the interface name from the string
+            match = re.search(r'ethernet-(\d+/\d+).(\d+)', line)
+            if match:
+                interface = match.group(1)
+                subinterface = match.group(2)
 
-        # Output the string
-        shutdown_command = f"sr_cli --candidate-mode --commit-at-end -- /interface ethernet-{interface} subinterface {subinterface} admin-state disable"
-        print(shutdown_command)
+                # Output the string
+                shutdown_command = f"sr_cli --candidate-mode --commit-at-end -- /interface ethernet-{interface} subinterface {subinterface} admin-state disable"
+                print(lower_line)
+                print(shutdown_command)
 
-        # Execute the command to enable the subinterface
-        os.system(shutdown_command)
+                # Execute the command to enable the subinterface
+                os.system(shutdown_command)
 
-        # Replace the line
-        new_line = re.sub(r'The number of MAC addresses in the bridge table for the sub-interface .* has reached the allowed limit of .*',
-                          f'The bridge table for the sub-interface ethernet-{interface}.{subinterface} has been shutdown.', line)
-        new_lines.append(new_line)
+                # Replace the line
+                new_line = re.sub(r'The number of MAC addresses in the bridge table for the sub-interface .* has reached the allowed limit of .*',
+                                  f'The bridge table for the sub-interface ethernet-{interface}.{subinterface} has been shutdown.', line)
+                new_lines.append(new_line)
     else:
         new_lines.append(line)
 
