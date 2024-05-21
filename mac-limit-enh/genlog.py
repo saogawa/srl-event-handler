96c036b467db48f2e6117ec2b0732183f56c0b71
Satoshi Ogawa <35554139+saogawa@users.noreply.github.com>
2024-05-21 14:44:21 +0900

    Update genlog.py
    For debugging

--- mac-limit-enh/genlog.py
+++ mac-limit-enh/genlog.py
@@ -1,2 +1,2 @@
-for i in range(1, 105):
+for i in range(1, 105): 
     print(f"2024-03-18T08:42:15.500032+00:00 D2L-edge4 sr_l2_mac_mgr: bridgetable|5585|5585|21302|W: The number of MAC addresses in the bridge table for the sub-interface ethernet-1/1.{i} has reached the allowed limit of 3.")
