db5c34b1d12076824a6de15b83d6ca9ced8b4daa
Satoshi Ogawa <35554139+saogawa@users.noreply.github.com>
2024-09-11 06:57:45 +0900

    Create mac-dup-enh3.py

---
+++ mac-dup-enh/mac-dup-enh3.py
@@ -0,0 +1,67 @@
+import sys
+import json
+
+# The main entry function for the event handler
+def event_handler_main(in_json_str):
+    # Parse the input JSON string passed by the event handler
+    in_json = json.loads(in_json_str)
+    paths = in_json["paths"]
+    options = in_json["options"]
+
+    # If the debug option is set to true, print the paths
+    if options.get("debug") == "true":
+        print(paths)
+
+    network_instance_names = []
+    for path in paths:
+        if "network-instance" in path["path"]:
+            network_instance_name = path["path"].split(" ")[1]
+            network_instance_names.append(network_instance_name)
+
+    print(f"Network instance name: {network_instance_names}")
+    response_actions = []
+
+    if network_instance_names is not None:
+        for network_instance_name in network_instance_names:
+            response_actions.append({
+                "set-tools-path": {
+                    "path": f"network-instance {network_instance_name} bridge-table mac-duplication delete-macs-type",
+                    "value": "all"
+                }
+            })
+
+    # If the debug option is set to true, print the response actions
+    if options.get("debug") == "true":
+        print(response_actions)
+
+    response = {"actions": response_actions}
+    return json.dumps(response)
+
+# Function to test the event handler_main function
+def main():
+    example_in_json_str = """
+{
+    "paths": [
+        {
+            "path": "network-instance mac-vrf10 bridge-table mac-duplication duplicate-entries mac 00:10:00:00:00:01 hold-down-time-remaining",
+            "value": "60"
+        },
+        {
+            "path": "network-instance mac-vrf20 bridge-table mac-duplication duplicate-entries mac 00:10:00:00:00:01 hold-down-time-remaining",
+            "value": "120"
+        },
+        {
+            "path": "network-instance mac-vrf30 bridge-table mac-duplication duplicate-entries mac 00:10:00:00:00:01 hold-down-time-remaining",
+            "value": "120"
+        }
+    ],
+    "options": {
+        "debug": "true"
+    }
+}
+"""
+    json_response = event_handler_main(example_in_json_str)
+    print(f"Response JSON:\n{json_response}")
+
+if __name__ == "__main__":
+    sys.exit(main())
